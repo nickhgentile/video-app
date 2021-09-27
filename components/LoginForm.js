2d2403817f8a24d99286f3d24bd1a83a50364ad7
Nick Gentile <nickhgentile@accenture.com>
2021-09-27 16:51:05 -0500

    Commenting out the auth0 for now.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -6,7 +6,7 @@ import { updateName, updateRoom, updateToken } from '../store/reducers/room';
 import Alert from './Alert';
 import Form from './Form';
 import TextInput from './TextInput';
-import {Login, Logout} from './Auth';
+// import { Login, Logout } from './Auth';
 
 export default function LoginForm() {
   const dispatch = useDispatch();
@@ -21,11 +21,6 @@ export default function LoginForm() {
     dispatch(updateName(data.name));
     dispatch(updateRoom(data.room));
     dispatch(updateToken(token.data.token));
-    // dispatch(
-    //   updateToken(
-
-    //   )
-    // );
 
     router.push('/room');
   };
@@ -52,8 +47,8 @@ export default function LoginForm() {
             placeholder="Room name"
           ></TextInput>
         </Form>
-        <Login/>
-        <Logout/>
+        {/* <Login/>
+        <Logout/> */}
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,22 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store from './../store';
 import '../styles/globals.css';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
 function App({ Component, pageProps }) {
   return (
-    <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}
-    >
-      <Provider store={store().store}>
-        <PersistGate loading={null} persistor={store().persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    </Auth0Provider>
+    // <Auth0Provider
+    //   domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+    //   clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+    //   redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}
+    // >
+
+    // </Auth0Provider>
+    <Provider store={store().store}>
+      <PersistGate loading={null} persistor={store().persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   );
 }
 
